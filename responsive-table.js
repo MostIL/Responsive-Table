0b48001a65aefebfc10ce27b4210b8326fbfeef3
Shimon Shore <ShimonShore@Yahoo.com>
2018-10-28 18:44:43 +0200

    Initial Upload

---
+++ responsive-table.js
@@ -0,0 +1,13 @@
+﻿
+$(".responsive-row").on("click", function () {
+	if ($(this).hasClass('row-open')) {
+		$(this).animate({
+			maxHeight: '65px'
+		}, 500).removeClass('row-open').find('.toggle-button').html('<i class=\"fas fa-angle-down\"></i>');
+	} else {
+		$(this).animate({
+			maxHeight: '1000px'
+		}, 500).addClass('row-open').find('.toggle-button').html('<i class=\"fas fa-angle-up\"></i>');
+	}
+});
+
